ede8f25e3bd2acee1696a3d0899b2aef0ae33b05
NdumisoTyam <Ndumisotyam1@gmail.com>
2025-09-16 18:38:07 +0200

    Update Subject.ts

--- 0x04-TypeScript/task_4/js/subjects/Subject.ts
+++ 0x04-TypeScript/task_4/js/subjects/Subject.ts
@@ -2,7 +2,7 @@ namespace Subjects {
   export class Subject {
     protected teacher!: Teacher;
 
-    setTeacher(teacher: Teacher): void {
+    setTeacher(teacher:Teacher): void {
       this.teacher = teacher;
     }
   }
